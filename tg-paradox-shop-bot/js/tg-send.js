838563f13e33abe3892ffc32ca1c28d6f72d8365
Paradox3576 <165952033+Paradox3576@users.noreply.github.com>
2024-04-12 21:56:58 +0400

    Update tg-send.js

--- tg-paradox-shop-bot/js/tg-send.js
+++ tg-paradox-shop-bot/js/tg-send.js
@@ -3,7 +3,8 @@ let tg = window.Telegram.WebApp
 function sendData() {
     tg.sendData(JSON.stringify({
         game: document.getElementsByTagName('h1')[0].innerText,
-        data: modalData
+        data: modalData,
+        description: document.getElementById("descriptions").innerText,
     }));
 }
 var BackButton = tg.BackButton;
