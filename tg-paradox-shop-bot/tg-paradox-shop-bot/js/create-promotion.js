88931566ae5283d2c8eab09b1e3aa23c8df730bc
Paradox3576 <165952033+Paradox3576@users.noreply.github.com>
2024-04-22 00:48:45 +0300

    Delete tg-paradox-shop-bot/tg-paradox-shop-bot directory

--- tg-paradox-shop-bot/tg-paradox-shop-bot/js/create-promotion.js
+++
@@ -1,8 +0,0 @@
-function createPromotion(i, image, price, title, description) {
-  appendCard(image, price, title, ["promotions"]);
-  const cards = document.querySelector('.promotions').querySelectorAll(".card");
-  console.log(cards);
-  cards[i].addEventListener('click', () => {
-    showModal(cards[i], description);
-  });
-}
