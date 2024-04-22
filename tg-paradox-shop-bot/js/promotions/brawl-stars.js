a1136fce9e6e8ee72ea4727a786488e1a392c137
Paradox3576 <165952033+Paradox3576@users.noreply.github.com>
2024-04-22 11:16:06 +0300

    Update brawl-stars.js

--- tg-paradox-shop-bot/js/promotions/brawl-stars.js
+++ tg-paradox-shop-bot/js/promotions/brawl-stars.js
@@ -1,3 +1,2 @@
-createPromotion(0, `../images/games/brawl-stars/Promotions/IMG_5727.jpeg`, 299, `800 Кредитов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
-createPromotion(1, `../images/games/brawl-stars/Promotions/IMG_5726.jpeg`, 299, `Боец Гром`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
-createPromotion(2, `../images/games/brawl-stars/Promotions/IMG_5728.jpeg`, 199, `Скин "Коммандо Скуик"`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(0, `../images/games/brawl-stars/Promotions/IMG_5749.jpeg`, 199, `90 Гемов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(1, `../images/games/brawl-stars/Promotions/IMG_5750.jpeg`, 299, `180 Гемов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
