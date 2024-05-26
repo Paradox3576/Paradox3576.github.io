e8febc90ccca4cb6846e5435bf2fec06fa1eb8a1
Paradox3576 <165952033+Paradox3576@users.noreply.github.com>
2024-05-26 16:12:51 +0400

    Update clash-of-clans.js

--- tg-paradox-shop-bot/js/promotions/clash-of-clans.js
+++ tg-paradox-shop-bot/js/promotions/clash-of-clans.js
@@ -8,3 +8,4 @@ createPromotion(6, `../images/games/clash-of-clans/promotions/IMG_5809.png`, 599
 createPromotion(7, `../images/games/clash-of-clans/promotions/IMG_5810.png`, 1149, `2000 Гемов + Хижина Строителя`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
 createPromotion(8, `../images/games/clash-of-clans/promotions/IMG_6474.jpeg`, 199, `Начальный Набор`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
 createPromotion(9, `../images/games/clash-of-clans/promotions/FDB1DB13-B0F6-4CE1-9CCE-F178155E4246.jpeg`, 599, `Набор Ратуши 5`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(10, `../images/games/clash-of-clans/promotions/IMG_7265.jpeg`, 799, `Набор Ратуши 7`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
