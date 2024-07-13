7a3864d19c4a33d43ab5e11b95302eb27b6388d5
Paradox3576 <165952033+Paradox3576@users.noreply.github.com>
2024-07-13 10:14:39 +0300

    Update clash-of-clans.js

--- tg-paradox-shop-bot/js/promotions/clash-of-clans.js
+++ tg-paradox-shop-bot/js/promotions/clash-of-clans.js
@@ -6,3 +6,4 @@ createPromotion(4, `../images/games/clash-of-clans/promotions/IMG_5810.png`, 114
 createPromotion(5, `../images/games/clash-of-clans/promotions/IMG_6474.jpeg`, 199, `Начальный Набор`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
 createPromotion(6, `../images/games/clash-of-clans/promotions/FDB1DB13-B0F6-4CE1-9CCE-F178155E4246.jpeg`, 599, `Набор Ратуши 5`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
 createPromotion(7, `../images/games/clash-of-clans/promotions/IMG_7265.jpeg`, 799, `Набор Ратуши 7`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(8, `../images/games/clash-of-clans/promotions/IMG_9049.jpeg`, 575, `Гоблинский Бассейн | Украшение`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
