07c02a7e607d6a7d01cc14847b716fd7215c999c
Paradox3576 <165952033+Paradox3576@users.noreply.github.com>
2024-09-14 12:24:50 +0400

    Update brawl-stars.js

--- tg-paradox-shop-bot/js/promotions/brawl-stars.js
+++ tg-paradox-shop-bot/js/promotions/brawl-stars.js
@@ -2,7 +2,7 @@ createPromotion(0, `../images/games/brawl-stars/Promotions/IMG_0647.png`, 549, `
 createPromotion(1, `../images/games/brawl-stars/Promotions/IMG_1038.png`, 2499, `30000 Крабсов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
 createPromotion(2, `../images/games/brawl-stars/Promotions/IMG_1144.png`, 999, `255 Гемов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
 createPromotion(3, `../images/games/brawl-stars/Promotions/IMG_1145.png`, 249, `4000 Блингов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
-createPromotion(4, `../images/games/brawl-stars/Promotions/IMG_1146.png`, 249, `Боец Хэнк`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
-createPromotion(5, `../images/games/brawl-stars/Promotions/IMG_1147.png`, 349, `Гиперзаряд "Притягивающий Прыжок"`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
-createPromotion(6, `../images/games/brawl-stars/Promotions/IMG_1038.png`, 349, `Гиперзаряд "В своей Стихии"`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
-createPromotion(7, `../images/games/brawl-stars/Promotions/IMG_1144.png`, 349, `1500 Кредитов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(4, `../images/games/brawl-stars/Promotions/IMG_1151.png`, 249, `Боец Хэнк`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(5, `../images/games/brawl-stars/Promotions/IMG_1152.png`, 349, `Гиперзаряд "Притягивающий Прыжок"`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(6, `../images/games/brawl-stars/Promotions/IMG_1153.png`, 349, `Гиперзаряд "В своей Стихии"`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
+createPromotion(7, `../images/games/brawl-stars/Promotions/IMG_1154.png`, 349, `1500 Кредитов`, `❇️ Быстрое выполнение заказа\n❇️ Покупка через официальный магазин\n❇️ Проверяйте наличие Акции`);
